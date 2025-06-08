ca0c32c00a847d6d17e25fb3aac191ab031052a4
Sanjith Soman <ks.sanjith@gmail.com>
2025-06-08 23:37:08 +0400

    fix: Add pg-connection-string for production db config

--- config/admin.ts
+++ config/admin.ts
@@ -1,9 +1,9 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'randomGeneratedKey'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('TRANSFER_TOKEN_SALT', 'randomSalt'),
   },
   transfer: {
     token: {
--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: '0.0.0.0',
-  port: 1337,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337), // fallback only
   app: {
     keys: env.array('APP_KEYS'),
   },
