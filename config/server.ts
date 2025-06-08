9bc7253c1c3f0b2345ce82852f3a3fbed6d9405d
Sanjith Soman <ks.sanjith@gmail.com>
2025-06-08 23:54:15 +0400

    fix: Add pg-connection-string for production db config

--- config/server.ts
+++ config/server.ts
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'NGROK_URL'),
+  url: env('URL'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
