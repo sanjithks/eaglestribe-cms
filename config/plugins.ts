de1f9b4d1b1b56ad09c5cc54e2aec1bf22a89cc5
Dr. Sanjith Soman <sanjith@inspirehealthscience.com>
2025-06-16 03:15:02 +0400

    cloudinary backend with values strapi v5 with v2

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,11 @@
-// ./config/plugins.ts
-
 export default () => ({
   upload: {
     config: {
       provider: '@strapi/cloudinary',
       providerOptions: {
-        cloud_name: process.env.diablculd,
-        api_key: process.env.622796691278471,
-        api_secret: process.env.sSDzMx_d01mVyK_2K_LY_rnT0Nk,
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
       },
     },
   },
