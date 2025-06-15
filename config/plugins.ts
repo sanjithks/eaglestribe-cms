a1b52f26f3e40e312d73c4ade1221aad79fd397d
Dr. Sanjith Soman <sanjith@inspirehealthscience.com>
2025-06-16 02:41:04 +0400

    cloudinary install

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('diablculd'),
+        api_key: env('622796691278471'),
+        api_secret: env('sSDzMx_d01mVyK_2K_LY_rnT0Nk'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
